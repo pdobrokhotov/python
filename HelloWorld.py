b0e9ecf8fccb9ee1d18d63339114e358bf1ee75b
Pavel Dobrokhotov <pdobrokhotov@gmail.com>
2022-10-01 12:37:41 +0300

    Ver.1.1.1

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,4 +1,18 @@
-print("Hello Python world!")
-print("Hola Mindo!")
-print("Hola!")
-print("Привет!")
+message = "Hello Python world!"
+#print(message)
+name = "test lowecase" 
+#print (name.title()) # = Test Lowecase  
+#print (name.upper()) # = TEST LOWECASE
+#print (name.lower()) # = test lowecase  
+#print (name + " " + message) # = test lowecase Hello Python world!
+#print("Languages:\nPython\nC\nJavaScript")  # adds nextline char
+#print("\tPython\nMy next line") # adds tab first and then nextline char
+#print("Languages:\n\tPython\n\tC\n\tJavaScript")
+# ========= striping spaces: rstrip)_,lstrip(),strip() ===========
+msg = "mys string ending with space and tab \t" 
+print (msg+ "suffix")
+print (msg.rstrip()  + "suffix")
+
+age = 23
+message = "Happy " + str(age) + "rd Birthday!" # use str() function to conver into to string
+print(message)
