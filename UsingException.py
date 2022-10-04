22eec4dc81672cbd6ced7d3c8cc8dbe3541526c0
Pavel Dobrokhotov <pdobrokhotov@gmail.com>
2022-10-04 21:10:43 +0300

    UsingException

---
+++ UsingException.py
@@ -0,0 +1,23 @@
+'''
+The try-except-else block works like this: Python attempts to run the
+code in the try statement. The only code that should go in a try statement
+is code that might cause an exception to be raised. Sometimes you have
+additional code that should run only if the try block was successful; 
+This code goes in the else block. The except block tells Python what to do in case
+a certain exception arises when it tries to run the code in the try statement.
+'''
+#=================================================================
+print("====== Catching exceptions: division by 0 ============") 
+try:
+    a = 5
+    b = 1   
+    answer = a/b
+    #answer+= "Result = "
+except ZeroDivisionError:
+    print("You can't divide by zero!")
+except TypeError:
+    print("You tried to concatenate string with number")
+else:
+    print(answer)
+#=================================================================
+    
