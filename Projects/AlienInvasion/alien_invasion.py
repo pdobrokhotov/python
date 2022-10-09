83abf0c0437d86cffea48461cf7434bf415034cb
Pavel Dobrokhotov <pdobrokhotov@gmail.com>
2022-10-09 19:58:26 +0300

    ThisCommotMustBeReverted

--- Projects/AlienInvasion/alien_invasion.py
+++ Projects/AlienInvasion/alien_invasion.py
@@ -71,3 +71,4 @@ def run_game():
 # Run MAIN function that STARTS GAME     
 run_game()
 #=========================================================================================
+# to be reverted!
