9b855d2a8e73be88ae96d1fa981b81f0d79dc5c6
Pavel Dobrokhotov <pdobrokhotov@gmail.com>
2022-10-16 14:24:12 +0300

    FinishedPage_443

--- Projects/Django/users/urls.py
+++ Projects/Django/users/urls.py
@@ -8,18 +8,24 @@ from django.urls import re_path as url
 from django.contrib.auth import views as login
 #----------------------------------------------------------------------------
 from . import views
-
+''' 
+When Django reads this URL, the word users tells Django 
+to look in users/urls.py, and login tells it to send requests to 
+Django's default login view (notice the view argument is login, 
+not views.login). Because we're not writing our own view function, 
+we pass a dictionary telling Django where to find the template we're 
+about to write. This template will be part of the users app, 
+not the learning_logs app. The line below from book is depricated:
+# Login page. 
+  url(r'^login/$', login, {'template_name': 'users/login.html'},name='login'),
+# Logout page
+  url(r'^logout/$', views.logout_view, name='logout'),
+'''
 urlpatterns = [
-    # Login page. When Django reads this URL, the word users tells Django 
-    # to look in users/urls.py, and login tells it to send requests to 
-    # Django’s default login view (notice the view argument is login, 
-    # not views.login). Because we’re not writing our own view function, 
-    # we pass a dictionary telling Django where to find the template we’re 
-    # about to write. This template will be part of the users app, 
-    # not the learning_logs app. The line below from book is depricated:
-    # url(r'^login/$', login, {'template_name': 'users/login.html'},name='login'),
-    #              And we use the below one instead:  
+    # Login page. 
     url(r'^login/$', 
-        login.LoginView.as_view(template_name='users/login.html'),name='login'), 
-
+        login.LoginView.as_view(template_name='users/login.html'),
+        name='login'), 
+    # Logout page
+    url(r'^logout/$', views.logout_view, name='logout'),
 ]
--- Projects/Django/users/views.py
+++ Projects/Django/users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
-
+'''
+The logout_view() function is straightforward: we just import Django's
+logout() function, call it, and then redirect back to the home page.
+Because module [django.core.urlresolvers import reverse] is deprocated
+we use module  [django.urls import reverse]
+'''
 # Create your views here.
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.urls import reverse
+from django.contrib.auth import logout
+#===================================================================
+# Log the user out.
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('learning_logs:index'))
+#===================================================================
